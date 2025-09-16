f2b3925242d155e37f94962df51ac437ecee8a21
PiotrO <piotro3425@gmail.com>
2025-09-16 10:47:32 +0200

    Connect to real arduino device to receive temperature

--- TempApp/Services/TempProvider.cs
+++ TempApp/Services/TempProvider.cs
@@ -1,17 +1,49 @@
-﻿namespace TempApp.Services
+﻿using System.Globalization;
+using System.IO.Ports;
+
+namespace TempApp.Services
 {
     public class TempProvider : ITempProvider
     {
-        private Random r;
+        SerialPort port;
+        private double currentTemp = -100d;
 
         public TempProvider()
         {
-            r = new Random();
+            port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            try
+            {
+                port.Open();
+            }
+            catch (Exception)
+            {
+                this.currentTemp = -101d;
+            }
+        }
+        ~TempProvider()
+        {
+            port.Close();
         }
 
-        public double GetTemp()
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
-            return (double)r.Next(0, 10000) / 100.0;
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadLine();
+            string valStr = indata.Substring(7).Trim(); ;
+
+            try
+            {
+                this.currentTemp = double.Parse(valStr, CultureInfo.InvariantCulture);
+            }
+            catch (FormatException)
+            {
+                this.currentTemp = -102d;
+            }
         }
+
+        public double GetTemp() => this.currentTemp;
     }
 }
