b784306728e98c023f2259be5f950e237258154f
PiotrO <piotro3425@gmail.com>
2025-09-29 14:54:05 +0200

    Add empty Avalonia client project

---
+++ TempClientAvalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TempClientAvalonia.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ TempClientAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TempClientAvalonia.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
