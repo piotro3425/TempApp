b960ec4739e558b9f983d296fa64290082f970d0
PiotrO <piotro3425@gmail.com>
2025-09-29 17:20:30 +0200

    Gets temp by button click

--- TempClientAvalonia/ViewModels/MainWindowViewModel.cs
+++ TempClientAvalonia/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,28 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using TempApp.Models;
 using TempClientAvalonia.Commands;
 
 namespace TempClientAvalonia.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string TempDisplayValue { get; } = "0";
+        private string tempDisplayValue = "0";
+
+        public string TempDisplayValue
+        {
+            get => tempDisplayValue;
+            set => this.SetProperty(ref tempDisplayValue, value);
+        }
         public string GetTempText { get; } = "Get Temp";
         public ICommand ExitCommand { get; set; }
+        public ICommand GetTempCommand { get; set; }
+
 
         public MainWindowViewModel()
         {
@@ -18,6 +31,28 @@ namespace TempClientAvalonia.ViewModels
                 if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
                     desktopLifetime.Shutdown();
             }, p => true);
+
+            GetTempCommand = new RelayCommand( async p => await GetTemp(p), p => true);
+        }
+
+        private async Task GetTemp(object? obj) => await GetTempFromServer();
+
+        private async Task GetTempFromServer()
+        {
+            try
+            {
+                HttpClient http = new HttpClient();
+                HttpResponseMessage res = await http.GetAsync("https://localhost:7182/api/Temp");
+                res.EnsureSuccessStatusCode();
+                Temp? temp = await res.Content.ReadFromJsonAsync<Temp>();
+
+                if (temp is not null)
+                    this.TempDisplayValue = $"{temp.Temperature} °C";
+            }
+            catch (HttpRequestException)
+            {
+                this.TempDisplayValue = "XXX";
+            }
         }
     }
 }
