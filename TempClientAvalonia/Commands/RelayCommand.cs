b637badc28bb53fd9ca84e542acdc0b90d38b3df
PiotrO <piotro3425@gmail.com>
2025-09-29 15:44:28 +0200

    Add first command that close application

---
+++ TempClientAvalonia/Commands/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TempClientAvalonia.Commands
+{
+    internal class RelayCommand(Action<object?> executeMethod, Func<object?, bool> canExecuteMethod) : ICommand
+    {
+        Action<object?> executeMethod = executeMethod;
+        Func<object?, bool> canExecuteMethod = canExecuteMethod;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            if (canExecuteMethod != null)
+                return canExecuteMethod(parameter);
+            else
+                return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (executeMethod != null)
+                executeMethod(parameter);
+        }
+    }
+}
--- TempClientAvalonia/ViewModels/MainWindowViewModel.cs
+++ TempClientAvalonia/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,23 @@
-﻿namespace TempClientAvalonia.ViewModels
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using System.Windows.Input;
+using TempClientAvalonia.Commands;
+
+namespace TempClientAvalonia.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        public string TempDisplayValue { get; } = "0";
+        public string GetTempText { get; } = "Get Temp";
+        public ICommand ExitCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ExitCommand = new RelayCommand(p =>
+            {
+                if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+                    desktopLifetime.Shutdown();
+            }, p => true);
+        }
     }
 }
